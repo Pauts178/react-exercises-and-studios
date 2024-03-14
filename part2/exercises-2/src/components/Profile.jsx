322117cfc3c9a3e717e54251fe367507be05ffaf
Pauts178 <katherine.pautler@gmail.com>
2024-03-14 13:33:36 -0500

    react part 2 exercises

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,14 +1,28 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json'
 
 
 function Profile()
 {
+   const listItem = oceans.map(oceans =>
+         <div key={oceans.id} className={`${oceans.fishCheck === 'true' ? 'isAFish' : 'profile'}`}>
+            <img src={oceans.image} alt={oceans.name} className='img'/>
+            <h1>{oceans.name}</h1>
+            <h2>Fun Facts</h2>
+               <ol>
+                  <li>{oceans.fact1}</li>
+                  <li>{oceans.fact2}</li>  
+                  <li>{oceans.fact3}</li>
+               </ol>
+            
+            <Button />
+         </div>
+      );
    return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+         <ul>
+            {listItem}
+         </ul>
    );
 }
 
